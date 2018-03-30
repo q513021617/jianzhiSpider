bfeef0f02bc3f5a686161f04ec56cf08fd2a6f4b
q513021617 <1871901600@qq.com>
2018-03-30 13:25:57 +0800

    修改了58同城的信息处理逻辑

--- jianzhi/excelDeal.py
+++ jianzhi/excelDeal.py
@@ -47,10 +47,43 @@ class JianzhiDeal(object):
         wb.close()
         pass
 
-
+    def deal58jianzhi(self,file):
+        wb=load_workbook(file)
+        sheet = wb.active
+        nowTime = datetime.datetime.now().strftime('%Y-%m-%d')
+        for row in sheet.rows:
+            flag=1
+            xrr=[]
+            for cell in row:
+                tempStr=cell.value
+                xrr.append(cell.value)
+                if tempStr is None:
+                    continue
+                if "发单" in tempStr.encode('UTF-8'):
+                    flag=0
+                    continue
+                if "传单" in tempStr.encode('UTF-8'):
+                    flag=0
+                    continue
+                if "客服" in tempStr.encode('UTF-8'):
+                    flag = 0
+                    continue
+                if "家教" in tempStr.encode('UTF-8'):
+                    flag = 0
+                    continue
+                if "老师" in tempStr.encode('UTF-8'):
+                    flag = 0
+                    continue
+            if flag!=1:
+                self.bootsheet.append(xrr)
+        self.workbook.save(nowTime+"新鲜出炉的兼职信息.xlsx")
+        wb.close()
+        pass
 if __name__ == '__main__':
     print "开始处理"
     dealClass=JianzhiDeal()
-    filenames=['58jianzhi.xlsx','doumijianzhi.xlsx','jiankejianzhi.xlsx','jianzhimaojianzhi.xlsx','jzbajianzhi.xlsx']
+    #'58jianzhi.xlsx',
+    filenames=['doumijianzhi.xlsx','jiankejianzhi.xlsx','jianzhimaojianzhi.xlsx','jzbajianzhi.xlsx']
+    dealClass.deal58jianzhi('58jianzhi.xlsx')
     for file in filenames:
         dealClass.dealExcel(file)
